07f1fc2e6a7f2cd064ef40edb310511b0370c109
Anthony Dalleggio <ad3002@nuy.edu>
2017-12-11 23:25:06 -0500

    Added PIFO wrapper with a programmable number of skip list instances Changed PIFO ordering to output min value first

---
+++ sw/python_sims/pifo_wrapper.py
@@ -0,0 +1,78 @@
+from __future__ import print_function
+import simpy
+import random
+from statistics import mean
+from pifo_simpy import SkipList
+
+NUM_SKIP_LISTS = 5
+PERIOD = 1
+NUM_NODES = 128
+
+class SkipListWrapper(SkipList):
+    def __init__(self, env, num_sl=NUM_SKIP_LISTS, period=PERIOD, size=NUM_NODES):
+        super(SkipListWrapper, self).__init__(env, period, size)
+        self.env = env
+        self.num_sl = num_sl
+        self.enq_in_pipe = simpy.Store(env)
+        self.enq_out_pipe = simpy.Store(env)
+        self.deq_in_pipe = simpy.Store(env)
+        self.deq_out_pipe = simpy.Store(env)
+
+        self.sl = []
+        self.num_entries = 0
+        
+        for i in range(num_sl):
+            self.sl.append(SkipList(env, period, size))
+
+        # register processes for simulation
+        self.run(env)
+
+    def run(self, env):
+        self.env.process(self.enqueue())
+        self.env.process(self.dequeue())
+
+    def enqueue(self):
+        while True:
+            # wait for enqueue command
+            enq_req = yield self.enq_in_pipe.get()
+            t1 = self.env.now
+            sel_sl = None
+            while sel_sl == None:
+                for i in range(self.num_sl):
+                    if (self.sl[i].busy == 0):
+                        if sel_sl == None:
+                            sel_sl = i
+                            min_num_entries = self.sl[i].num_entries
+                        else:
+                            if self.sl[i].num_entries < min_num_entries:
+                                sel_sl = i
+                                min_num_entries = self.sl[i].num_entries
+                if sel_sl == None:
+                    yield self.env.timeout(PERIOD)
+            self.sl[sel_sl].enq_in_pipe.put(enq_req)
+            self.enq_out_pipe.put(self.env.now - t1)
+            self.num_entries += 1
+
+
+    def dequeue(self):
+        while True:
+            # wait for dequeue request
+            deq_req = yield self.deq_in_pipe.get()
+            t1 = self.env.now
+            sel_sl = None
+            while sel_sl == None:
+                for i in range(self.num_sl):
+                    if (self.sl[i].avail == 1):
+                        if sel_sl == None:
+                            sel_sl = i
+                            min_value = self.sl[i].value
+                        else:
+                            if self.sl[i].value < min_value:
+                                sel_sl = i
+                                min_value = self.sl[i].value
+                if sel_sl == None:
+                    self.env.timeout(PERIOD)
+            self.num_entries -= 1
+            self.sl[sel_sl].deq_in_pipe.put(deq_req)
+            deq_resp = yield self.sl[sel_sl].deq_out_pipe.get()
+            self.deq_out_pipe.put(deq_resp)
---
+++ sw/python_sims/test_pifo_wrapper.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+import simpy
+import random
+from statistics import mean
+from pifo_wrapper import SkipListWrapper
+
+def test(env):
+    NUM_SKIP_LISTS = 3
+    PERIOD = 1
+    MAX_NODES = 256
+    CLK_FREQ = 200 # MHz
+    PKT_RATE = 14.8 # MPkts/sec
+    PKT_INTERVAL = int(CLK_FREQ/PKT_RATE)
+    NumRuns = 10
+    NumOps = 64
+    enq_nclks_list = []
+    deq_nclks_list = []
+    
+    slw = SkipListWrapper(env, num_sl=NUM_SKIP_LISTS, period=PERIOD, size=MAX_NODES)
+    print ('@ {:04d} - starting skip list init'.format(env.now))
+    yield env.timeout(40)
+    print ('@ {:04d} - done skip list init'.format(env.now))
+    
+    for j in range(NumRuns):
+        print ("Run:", j)
+        # Enqueue some values and print skip list
+        for i in range (NumOps):
+            val = random.randint(0,100)
+            hsp = mdp = -1
+            t1 = env.now
+            slw.enq_in_pipe.put((val, hsp, mdp))
+            enq_nclks = yield slw.enq_out_pipe.get()
+            enq_nclks_list.append(enq_nclks)
+            print ('enq: {} - {} clks'.format(val, enq_nclks))
+            yield env.timeout(PKT_INTERVAL)
+        
+        # Dequeue all values and print skip list
+        while slw.num_entries > 0:
+            slw.deq_in_pipe.put(True)
+            (val, hsp, mdp, deq_nclks) = yield slw.deq_out_pipe.get()
+            print ('deq: {} - {} clks'.format(val, deq_nclks))
+            deq_nclks_list.append(deq_nclks)
+            yield env.timeout(PKT_INTERVAL)
+
+    print ("Time measurements (min, avg, max) for {} runs of {} enq/deq ops".format(NumRuns, NumOps))
+    print ("Enq:", min(enq_nclks_list), mean(enq_nclks_list), max(enq_nclks_list))
+    print ("Deq:", min(deq_nclks_list), mean(deq_nclks_list), max(deq_nclks_list))
+
+
+# Main
+env = simpy.Environment()
+env.process(test(env))
+env.run()
