217b8865b64245ead333eb77d39ee28f4295dac7
Stephen Ibanez <sibanez@g.hmc.edu>
2018-01-13 17:00:01 -0800

    Updated the test_pifo_pkt_storage.py to send scapy pkts with the AXI4StreamMaster module in the cocotb repo

--- simple_sume_switch/hw/hdl/pkt_storage/cocotb_tests/test_pifo_pkt_storage.py
+++ simple_sume_switch/hw/hdl/pkt_storage/cocotb_tests/test_pifo_pkt_storage.py
@@ -3,14 +3,22 @@ import logging
 import cocotb
 
 from cocotb.clock import Clock
-from cocotb.triggers import Timer, RisingEdge, ClockCycles
+from cocotb.triggers import Timer, ReadOnly, RisingEdge, ClockCycles
+from cocotb.binary import BinaryValue
+from cocotb.axi4stream import AXI4StreamMaster, AXI4StreamSlave
+
+from Tuser import Tuser
+from scapy.all import Ether, IP, UDP
+# Supress SCAPY warning messages
+logging.getLogger("scapy").setLevel(logging.ERROR)
 
 PERIOD = 5000
 
 @cocotb.test()
 def test_pifo_pkt_storage(dut):
-    """Test plus_one module in HW simulation
+    """Testing pifo_pkt_storage module
     """
+    # start HW sim clock
     cocotb.fork(Clock(dut.axis_aclk, PERIOD).start())
 
     # Wait to start
@@ -22,16 +30,30 @@ def test_pifo_pkt_storage(dut):
     dut.m_axis_pkt_tready <= 1
     dut._log.debug("Out of reset")
 
-    dut.s_axis_pkt_tvalid <= 0
-    dut.s_axis_pkt_tdata <= 0
-    dut.s_axis_pkt_tkeep <= 0
-    dut.s_axis_pkt_tuser <= 0
-    dut.s_axis_pkt_tlast <= 0
-
     dut.s_axis_ptr_tvalid <= 0
     dut.s_axis_ptr_tdata <= 0
     dut.s_axis_ptr_tkeep <= 0
     dut.s_axis_ptr_tlast <= 0
+
+    # Attach and AXI4Stream Master
+    pkt_master = AXI4StreamMaster(dut, 's_axis_pkt', dut.axis_aclk, data_width=32, user_width=16)
+
+    # build a packet
+    pkt = Ether(dst='ff:ff:ff:ff:ff:ff', src='08:22:22:22:22:08')
+    pkt = pkt / ('\x00'*(160 - len(pkt)))
+
+    # build the metadata 
+    meta = Tuser(dst_port=0b00000100, src_port=0b00000001, pkt_len=len(pkt))
+    tuser = BinaryValue(bits=len(meta)*8, bigEndian=False)
+    tuser.set_buff(str(meta))
+
+    yield pkt_master.write_pkts([pkt, pkt], [tuser, tuser])
+
+#    dut.s_axis_pkt_tvalid <= 0
+#    dut.s_axis_pkt_tdata <= 0
+#    dut.s_axis_pkt_tkeep <= 0
+#    dut.s_axis_pkt_tuser <= 0
+#    dut.s_axis_pkt_tlast <= 0
  
     yield ClockCycles(dut.axis_aclk, 100)
 
