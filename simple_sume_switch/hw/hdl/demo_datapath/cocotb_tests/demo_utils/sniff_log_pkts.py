fe67bb74fa349454692182a6e66fbdb7a462d78f
Stephen Ibanez <sibanez@g.hmc.edu>
2018-05-22 13:17:39 -0700

    Adding tool to sniff and print logged pkts

---
+++ simple_sume_switch/hw/hdl/demo_datapath/cocotb_tests/demo_utils/sniff_log_pkts.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import argparse
+from scapy.all import *
+from log_pkt_parser import LogPktParser
+
+def show_log_pkt(pkt):
+    pkt_buf = str(pkt)
+    # Parse the logged pkt
+    pkt_parser = LogPktParser()
+    log_pkt = pkt_parser.parse_pkts(pkt_buf)
+    print 'Recived Pkt: {}'.format(str(log_pkt))
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('iface', type=str, help="the interfaces to sniff on")
+    args = parser.parse_args()
+
+    sniff(iface=args.iface, prn=show_log_pkt, count=0)
+
+
+if __name__ == '__main__':
+    main()
+
