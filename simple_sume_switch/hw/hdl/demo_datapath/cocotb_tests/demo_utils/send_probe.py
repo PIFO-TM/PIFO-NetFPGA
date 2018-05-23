b8f3c6683925dae55d2bbfd76ca9b9518b840a46
Stephen Ibanez <sibanez@g.hmc.edu>
2018-05-23 13:12:06 -0700

    Updated the send_probe tool to send a reset pkt if desired

--- simple_sume_switch/hw/hdl/demo_datapath/cocotb_tests/demo_utils/send_probe.py
+++ simple_sume_switch/hw/hdl/demo_datapath/cocotb_tests/demo_utils/send_probe.py
@@ -7,9 +7,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('iface', type=str, help="the interface to send out of")
     parser.add_argument('--sport', type=int, default=0, help="the TCP sport field to use")
+    parser.add_argument('--reset', action='store_true', default=False, help="send pkt to reset RR rank pipe state")
     args = parser.parse_args()
 
-    pkt = Ether(dst='08:11:11:11:11:08', src='08:22:22:22:22:08') / IP(src='10.0.0.2', dst='10.0.0.1') / TCP(sport=args.sport) / ('\x00'*10)
+    eth_src = 'ff:ff:ff:ff:ff:ff' if args.reset else '08:22:22:22:22:08'
+    pkt = Ether(dst='08:11:11:11:11:08', src=eth_src) / IP(src='10.0.0.2', dst='10.0.0.1') / TCP(sport=args.sport) / ('\x00'*10)
     sendp(pkt, iface=args.iface)
 
 
